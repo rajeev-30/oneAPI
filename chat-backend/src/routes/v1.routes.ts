11f0707530b3b588a6c7acef102fc8289f18c513
rajeev-30 <rajeevkushwaha1812@gmail.com>
2026-05-02 00:14:11 +0530

    delete chat-backedn folder

--- chat-backend/src/routes/v1.routes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-
-const app = express();
-
-// app.use("/user", userRoute);
-
-
-export default app;
--- chat-backend/src/types/types.ts
+++
@@ -1,6 +0,0 @@
-export type ApiResponse<T = unknown> = {
-    success: boolean;
-    message: string;
-    data?: T | null;
-    error?: unknown;
-};
